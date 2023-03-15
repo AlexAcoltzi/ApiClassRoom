3f9665ad2fccac008e59f648aba81c65bab0b2e5
alexf <alex.fantastico22@gmail.com>
2023-03-15 09:43:21 -0600

    Creando tabla AlumnoTarea

---
+++ src/main/java/com/buap/alex/backendbuapclassroom/Controller/TareaController.java
@@ -0,0 +1,47 @@
+package com.buap.alex.backendbuapclassroom.Controller;
+
+import com.buap.alex.backendbuapclassroom.Domain.Curso;
+import com.buap.alex.backendbuapclassroom.Domain.Tarea;
+import com.buap.alex.backendbuapclassroom.repository.CursoRepository;
+import com.buap.alex.backendbuapclassroom.repository.TareaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Tarea")
+public class TareaController {
+
+    @Autowired
+    TareaRepository repository;
+    CursoRepository cursoRepository;
+
+    @PostMapping("/crearTarea")
+    public ResponseEntity<?> crearTarea(@RequestBody Tarea tarea, @RequestParam  Long nrc){
+        Curso curso= cursoRepository.findCursoByNRC(nrc).get();
+        String Ruta = createDirectory(curso.getRuta(), tarea.getNombre());
+        tarea.setRuta(Ruta);
+        repository.save(tarea);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private static String createDirectory(String ruta, String name){
+        String PATH = ruta;
+        String directoryName = PATH.concat(name);
+
+
+        File directory = new File(directoryName);
+        if (!directory.exists()){
+            if (directory.mkdirs()){
+                System.out.println("Directorio creado " + directoryName);
+            } else{
+                System.out.println("Failed to create directory!");
+            }
+        }
+        return directoryName;
+    }
+}
---
+++ src/main/java/com/buap/alex/backendbuapclassroom/Domain/AlumnoTarea.java
@@ -0,0 +1,31 @@
+package com.buap.alex.backendbuapclassroom.Domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@Table(name = "alumno_tarea")
+@ToString
+public class AlumnoTarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Getter
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_user", referencedColumnName = "idUser")
+    @Getter @Setter
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_tarea", referencedColumnName = "idtarea")
+    @Getter @Setter
+    private Tarea tarea;
+
+    @Column(name = "calificacion")
+    @Getter @Setter
+    private int calificacion;
+}
